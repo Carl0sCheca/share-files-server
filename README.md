44ff9e4c9de30a20c74859cd670145ad32973efe
Carlos Checa <carloschecaa@gmail.com>
2023-11-23 22:26:32 +0100

    filenames and shorter hash

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ This server works together with [share-files-client](https://github.com/Carl0sCh
 You can also upload files using curl:
 
 ```bash
-curl -X POST -H "format:{file_format}" -H "share-token:{token}" --data-binary "@{/path/to/file}" {server_url}/upload
+curl -X POST -H "share-filename:{filename.format}" -H "share-token:{token}" --data-binary "@{/path/to/file}" {server_url}/upload
 ```
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use actix_web::{
 };
 
 use chrono::{DateTime, Local};
-use minio_rsc::{provider::StaticProvider, Minio};
+use minio_rsc::{client::Tags, provider::StaticProvider, Minio};
 
 const BUCKET_NAME: &str = "share-files";
 
@@ -33,9 +33,44 @@ fn minio_provider() -> StaticProvider {
 async fn upload(req: HttpRequest, payload: actix_web::web::Bytes) -> impl Responder {
     let secret_token = std::env::var("SECRET_TOKEN").unwrap();
 
-    let format = match req.headers().clone().get("format") {
-        Some(value) => String::from_utf8(value.as_bytes().to_vec()).unwrap(),
-        None => "txt".to_owned(),
+    let filename = req
+        .headers()
+        .clone()
+        .get("share-filename")
+        .map(|value| String::from_utf8(value.as_bytes().to_vec()).unwrap());
+
+    let format = {
+        if let Some(file_name) = filename.clone() {
+            let parts: Vec<&str> = file_name.split('.').collect();
+
+            if !parts.is_empty() {
+                if let Some(extension) = parts.last() {
+                    match *extension {
+                        ">sc" => "png".to_owned(),
+                        ">txt" => "txt".to_owned(),
+                        value => match parts.len() {
+                            2.. => value.to_owned(),
+                            _ => "".to_owned(),
+                        },
+                    }
+                } else {
+                    "txt".to_owned()
+                }
+            } else {
+                "txt".to_owned()
+            }
+        } else {
+            "txt".to_owned()
+        }
+    };
+
+    let filename = match filename {
+        Some(name) => match name.as_str() {
+            ">sc" => None,
+            ">txt" => None,
+            value => Some(value.to_owned()),
+        },
+        None => None,
     };
 
     match req.headers().clone().get("share-token") {
@@ -47,16 +82,18 @@ async fn upload(req: HttpRequest, payload: actix_web::web::Bytes) -> impl Respon
         }
     };
 
-    let filename = {
+    let filename_hash = {
         let now = Local::now();
         let timestamp_str = now.to_string();
         let mut hasher = sha1_smol::Sha1::new();
         hasher.update(timestamp_str.as_bytes());
         let result: String = hasher.digest().to_string();
 
+        let short_result = result[0..10].to_owned();
+
         match format {
-            x if x.is_empty() => result,
-            format => format!("{result}.{format}"),
+            x if x.is_empty() => short_result,
+            format => format!("{short_result}.{format}"),
         }
     };
 
@@ -83,20 +120,29 @@ async fn upload(req: HttpRequest, payload: actix_web::web::Bytes) -> impl Respon
     }
 
     minio
-        .put_object(BUCKET_NAME, filename.to_string(), payload)
+        .put_object(BUCKET_NAME, filename_hash.to_string(), payload)
         .await
         .unwrap();
+
+    if let Some(filename) = filename {
+        let mut tags: Tags = Tags::new();
+        tags.insert("filename", filename);
+        minio
+            .set_object_tags(BUCKET_NAME, filename_hash.to_string(), tags)
+            .await
+            .unwrap();
+    }
 
     let conn_info = req.connection_info();
     let url = format!("{}://{}", conn_info.scheme(), conn_info.host());
 
     println!(
         "{}",
-        info(format!("A new file has been received: {filename}").as_str())
+        info(format!("A new file has been received: {filename_hash}").as_str())
     );
 
     web::Json(UploadResponse::Ok {
-        message: format!("{url}/{filename}"),
+        message: format!("{url}/{filename_hash}"),
     })
 }
 
@@ -157,10 +203,23 @@ async fn get_file(path: web::Path<String>) -> impl Responder {
         .build()
         .unwrap();
 
-    if let Ok(value) = minio.get_object(BUCKET_NAME, file_name).await {
-        HttpResponse::Ok()
-            .content_type(mime_type)
-            .body(value.bytes().await.unwrap())
+    if let Ok(value) = minio.get_object(BUCKET_NAME, file_name.clone()).await {
+        let tags: Tags = minio.get_object_tags(BUCKET_NAME, file_name).await.unwrap();
+
+        if tags.contains_key("filename") {
+            let filename = tags["filename"].clone();
+            HttpResponse::Ok()
+                .append_header((
+                    "Content-Disposition",
+                    format!("inline; filename=\"{}\"", filename),
+                ))
+                .content_type(mime_type)
+                .body(value.bytes().await.unwrap())
+        } else {
+            HttpResponse::Ok()
+                .content_type(mime_type)
+                .body(value.bytes().await.unwrap())
+        }
     } else {
         HttpResponse::NotFound()
             .content_type("text/plain")
